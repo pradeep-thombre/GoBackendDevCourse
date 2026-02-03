2b9f982b0eaa1075879f6196f6f6e90a5b559071
padiee <pradeepbthombre@gmail.com>
2026-02-03 17:58:46 +0530

    added day 4.

---
+++ Day04/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var num1, num2, num3 int
+	// fmt.Println("Enter 3 Num")
+	// fmt.Scan(&num1, &num2, &num3)
+	// fmt.Println("You have entered num1", num1)
+	// fmt.Println("You have entered num2", num2)
+	// fmt.Println("You have entered num3", num3)
+	// add := num1 + num2 + num3
+	// fmt.Println("The addition is:", add)
+
+	// var name string
+	// var age string
+	// fmt.Println("Enter your name:")
+	// fmt.Scan(&name)
+	// fmt.Println("Enter you age")
+	// fmt.Scan(&age)
+	// fmt.Println("You have entered name:", name)
+	// fmt.Println("You have entered age:", age)
+
+	// var num1, num2 int
+	// fmt.Println("Enter 2 Num")
+	// fmt.Scanln(&num1, &num2)
+	// fmt.Println("You have entered num1", num1)
+	// fmt.Println("You have entered num2", num2)
+
+	// var name string
+	// var age int
+	// var istrue bool
+	// fmt.Println("Enter your name")
+	// fmt.Scanln(&name, &age, &istrue)
+	// fmt.Println("You have entered name:", name)
+	// fmt.Println("You have entered age:", age)
+	// fmt.Println("You have entered istrue:", istrue)
+
+	// var isTrue bool
+	// fmt.Println("Enter true or false")
+	// fmt.Scanln(&isTrue)
+	// fmt.Println("You have entered isTrue:", isTrue)
+
+	var myFloat float32
+	fmt.Println("Enter a float value")
+	fmt.Scanln(&myFloat)
+	fmt.Println("You have entered float value:", myFloat)
+
+}
